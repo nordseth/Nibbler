392ca096e2164971d8b5f294c2ebedc154cb2da3
Andreas Nordseth <anordseth@gmail.com>
2022-08-16 22:36:02 +0200

    remove expires in, as it breaks system.text.json and is not used

--- Nibbler/Utils/AuthenticationHandler.cs
+++ Nibbler/Utils/AuthenticationHandler.cs
@@ -184,7 +184,6 @@ namespace Nibbler.Utils
             public string token { get; set; }
             public string refresh_token { get; set; }
             public string access_token { get; set; }
-            public string expires_in { get; set; }
             public string scope { get; set; }
         }
     }
