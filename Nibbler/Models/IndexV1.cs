c8528b8323b1870b539360fd1fdc9927b801d904
Andreas Nordseth <anordseth@gmail.com>
2023-04-12 23:25:53 +0200

    add support for reading from images indexes (defaults to linux-amd64)

---
+++ Nibbler/Models/IndexV1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nibbler.Models
+{
+    /// <summary>
+    /// https://github.com/opencontainers/image-spec/blob/main/image-index.md
+    /// </summary>
+    public class IndexV1
+    {
+        public const string MimeType = "application/vnd.oci.image.index.v1+json";
+        public const string AltMimeType = "application/vnd.docker.distribution.manifest.list.v2+json";
+
+        public int schemaVersion { get; set; } = 2;
+        public string mediaType { get; set; } = MimeType;
+        public List<IndexV1Manifest> manifests { get; set; }
+        public Dictionary<string, string> annotations { get; set; }
+    }
+
+    /// <summary>
+    /// https://github.com/opencontainers/image-spec/blob/main/image-index.md
+    /// and https://github.com/opencontainers/image-spec/blob/main/descriptor.md#properties
+    /// </summary>
+    public class IndexV1Manifest
+    {
+        public string mediaType { get; set; } = ManifestV2.MimeType;
+        public string digest { get; set; } 
+        public long size { get; set; }
+        public List<string> urls { get; set; }
+        public Dictionary<string, string> annotations { get; set; }
+        public string data { get; set; }
+        public string artifactType { get; set; }
+        public IndexV1Platform platform { get; set; }
+    }
+
+    public class IndexV1Platform
+    {
+        public const string DefaultOS = "linux";
+        public const string DefaultArch = "amd64";
+
+        public string architecture { get; set; } = DefaultOS;
+        public string os { get; set; } = DefaultArch;
+        public string variant { get; set; }
+        public List<string> features { get; set; }
+    }
+}
--- Nibbler/Utils/JsonContext.cs
+++ Nibbler/Utils/JsonContext.cs
@@ -11,6 +11,7 @@ namespace Nibbler.Utils
     [JsonSourceGenerationOptions(WriteIndented = true, DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonSerializable(typeof(ManifestV2))]
     [JsonSerializable(typeof(ImageV1))]
+    [JsonSerializable(typeof(IndexV1))]
     [JsonSerializable(typeof(AuthenticationHandler.TokenResponse))]
     [JsonSerializable(typeof(DockerConfigCredentials.DockerConfig))]
     [JsonSerializable(typeof(DockerConfigCredentials.CredentialHelperResult))]
